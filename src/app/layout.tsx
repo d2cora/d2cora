693a88ec17a0d553e7bdcbd4f240f406701be585
Rahul Bora <borarahul623436@gmail.com>
2026-02-17 16:47:22 +0530

    GTM added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,13 +29,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark scroll-smooth" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MP7SS4HT');`}} />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={cn(
           inter.variable,
           spaceGrotesk.variable,
-          "font-sans min-h-screen antialiased selection:bg-white/20"
+          "min-h-screen font-sans antialiased selection:bg-white/20"
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MP7SS4HT"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll />
         <Navbar />
         {children}
