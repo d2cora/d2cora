611a520325c96de296d4bb8b27dbe98f1c3e39c8
Rahul Bora <borarahul623436@gmail.com>
2026-06-17 17:54:37 +0530

    removed linis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-import { SmoothScroll } from "@/components/SmoothScroll";
+
 import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
@@ -73,7 +73,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-        <SmoothScroll />
+
         <Navbar />
         {children}
         <Footer />
