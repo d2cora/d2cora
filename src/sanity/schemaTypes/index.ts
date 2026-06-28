7e61155ece69c0033a022903ca45e11fb5be24e2
Rahul Bora <borarahul623436@gmail.com>
2026-06-28 11:09:17 +0530

    dynamic sitmap generated

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,8 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import {postType} from './postType'
 import {testimonialType} from './testimonialType'
-import {faqType} from './faqType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType, testimonialType, faqType],
+  types: [postType, testimonialType],
 }
+
